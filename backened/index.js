097186a4bbdb9b95578bdca6f6b4b335cf573092
AhmedReda-662 <ahmed136227reda@gmail.com>
2026-06-18 20:12:17 +0300

    fix serverless function

--- backened/index.js
+++ backened/index.js
@@ -7,46 +7,36 @@ const dbConnection = require("./config/dbConfig");
 
 // routes
 const imageRoute = require("./api/uploadRoute");
+
 // error handling
 const ApiError = require("./utils/apiError");
 const globalError = require("./middleware/errorMiddleware");
 
-// register config
 dotenv.config({ path: "config.env" });
 
 // connect db
 dbConnection();
+
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-  console.log(`Mode: ${process.env.NODE_ENV}`);
 }
 
-// mount routes
+// routes
 app.use("/api/v1", imageRoute);
 
+// 404 handler
 app.use((req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
-// Global error handling middleware
+// global error handler
 app.use(globalError);
 
-// run server
-const server = app.listen(process.env.PORT || 8000, (req, res) => {
-  console.log(`App running on port ${process.env.PORT || 8000}`);
-});
-
-// handle error outside the reigon of express
-process.on("unhandledRejection", (err) => {
-  console.log(`UnhandledRejection Erros: ${err.name} | ${err.message}`);
-  server.close(() => {
-    console.log("Shutting down...");
-    process.exit(1);
-  });
-});
+module.exports = app;
